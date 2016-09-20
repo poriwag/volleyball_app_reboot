ea366a7796619a02cbe9eec580acd901ace43cab
Billy <pak.billy@gmail.com>
2016-09-20 17:24:03 +0000

    implement advanced login with remember me feature

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,11 +19,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
+    #simulate a user clicking outout in a second window
+    delete logout_path
     assert_redirected_to root_url
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1' )
+    assert_not_nil cookies['remember_token']
+  end
+  
+  test "login wtihout rmeembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
   
   
